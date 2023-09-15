172fd51e594d499be34a2b651c08e510e5d14ff5
kwon93 <93umsongaji@gmail.com>
2023-09-15 20:26:25 +0900

    study: useState

--- 01-reactBasic/src/components/Expenses/ExpenseItem.js
+++ 01-reactBasic/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import React, {useState} from 'react';
+
+
+
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.title);
+
+
+    const clickHandler = () =>{
+        setTitle("Updated!"); //setTitle()를 통해 ExpenseItem 를 다시 호출한다.
+    };
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card> 
         );
     }
